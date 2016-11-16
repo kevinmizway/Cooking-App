1b7bf301af3e41e4dc516f5e1ad6acff7475196c
Kevin Mizway <mizzledragon@gmail.com>
2016-11-15 23:53:14 -0500

    Add gallons to liters html form with jquery to display in hidden solution.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
-var convertToLiters = function(gallons) {
-  return "This is equal to " + (gallons * 3.78541) + " liters.";
+//Business Logic
+var gallonsToLiters = function(gallons) {
+  return gallons * 3.78541;
 }
 
-var gallons = parseInt(prompt("Please provide how many gallons you wish to convert to liters."));
-var convertedResult = convertToLiters(gallons);
+var squareFeetToSquareMeters = function(feet) {
+  return feet * .093;
+}
 
-alert(convertedResult);
+var inchToCM = function (inch) {
+  return inch * 2.54;
+}
+
+var fahrenheitToCelsius = function(fahrenheit) {
+  return (fahrenheit - 32) / 1.8;
+}
+
+//Front-End Logic
+
+$(function() {
+  $("form#gallonsToLiters").submit(function(event) {
+    event.preventDefault();
+
+    var gallons = parseInt($("#gallons").val());
+    var liters = gallonsToLiters(gallons);
+
+    $(".gallons").text(gallons);
+    $(".liters").text(liters);
+    $("#gallons-solution").show();
+  });
+});
